fdc46b69926512a6f100f9f821fcf0e6ae905911
Daniel Alejandro Cast <lexcast13@gmail.com>
2015-03-05 18:29:33 -0700

    added id variable with setter and getter

--- framework/Fminor/Core/Request/RequestAbstract.php
+++ framework/Fminor/Core/Request/RequestAbstract.php
@@ -3,11 +3,22 @@ namespace Fminor\Core\Request;
 
 abstract class RequestAbstract
 {
+    private $id;
     static function filter($requests)
     {
         return array_filter($requests, function ($request) {
             $class = get_class();
             return ($request instanceof $class);
         });
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
     }
 }
